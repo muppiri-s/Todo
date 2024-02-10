08da91db4684c216d0d74989364c8e1d9dff69b8
Sahithi Muppiri <mupps02@pfw.edu>
2024-02-10 18:16:05 -0500

    second commit

--- views.py
+++ views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ToDoItem
+
 
 # Create your views here.
+class AllToDos(ListView):
+    model = ToDoItem
+    template_name = "todo/index.html"
+
+
+class TodayToDos(ListView):
+    model = ToDoItem
+    template_name = "todo/today.html"
+
+    def get_queryset(self):
+        return ToDoItem.objects.filter(due_date=date.today())
